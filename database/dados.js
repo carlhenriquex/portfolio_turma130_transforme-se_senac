1c1bcf1d24a02d0eb8cc9c54a6bd468d0a54f893
Sabrina <sabrinabarros0102@gmail.com>
2026-08-12 00:00:02 -0300

    atualizei dados dos alunos

--- database/dados.js
+++ database/dados.js
@@ -1,131 +1,147 @@
 // Lista de alunos
 const alunos = [
-  {
+
     // Dicionário
     //Aluno 1
-
+  {
+    foto: "img/adriele.jpg",
     nome: "Adrielly Alexandra Cordeiro Velozo",
     formacoes: ["Análise e Desenvolvimento de Sistemas"],
-    experiencia: "Hackathon, Auditoria de Qualidade",
+    experiencia: ["Hackathon", "Auditoria de Qualidade"],
     interesses: ["Back-End Developer", "Cybersegurança", "Design"],
     github: "https://github.com/adriellyvelozotech",
     linkedin: "https://www.linkedin.com/in/adriellyvelozotech/"
-  },
+},
     //Aluno 2
-  {
+{
+  foto:"img/andrea.jpg",
   nome: "Andréa Verônica Silva Menezes",
   formacoes: ["Informática Básica e Desenvolvimento de Sistemas"],
-  experiencia: "Marketing Digital",
-  interesses: ["Desenvolvimento de Sistemas, Design"],
+  experiencia: ["Marketing Digital"],
+  interesses: ["Desenvolvimento de Sistemas","Design"],
   github: "Andreaveronica20003-fonte (Andréasilvaa)",
   linkedin: "Andreaa Veronica | LinkedIn"
 },
-// aluno 3
+    // aluno 3
 {
-    
+  foto: "img/caio.jpg",
+  nome: "Caio Cesar Costa",
+  formacoes: ["Ciência da Computação"],
+  experiencia: ["Hackathons", "Ideathons","Projetos Frontend"],
+  interesses: ["Fullstack development "],
+  github: "https://www.linkedin.com/in/caioccesar/",
+  linkedin: "https://github.com/caiooozs"
+},
+    //Aluno 4
+{   
+  foto: "img/carlos.jpg",
   nome: "Carlos Henrique Correia dos Santos",
-  formacoes: ["Técnico em Desenvolvimento de Sistemas e em Design Gráfico, Graduando Ciência da Computação"],
-  experiencia: "Professor de DevOps",
-  interesses: ["Back-end developer, DevOps engineer"],
+  formacoes: ["Técnico em Desenvolvimento de Sistemas", "Design Gráfico", "Graduando Ciência da Computação"],
+  experiencia: ["Professor de DevOps"],
+  interesses: ["Back-end developer", "DevOps engineer"],
   github: "https://github.com/carlhenriquex",
   linkedin: "https://www.linkedin.com/in/carloshenriquex"
 },
-    //Aluno 4
+    //Aluno 5
 {
+  foto: "img/dacyrrose.jpg",
   nome: "Dacyrrôse Pessoa Salvador de Melo",
-  formacoes: "Análise e Desenvolvimento de Sistemas",
-  experiencia: "Hackathons e Ideathons",
-  interesses: ["Desenvolvimento Full-Stack / RPA / QA"],
+  formacoes: ["Análise e Desenvolvimento de Sistemas"],
+  experiencia: ["Hackathons", "Ideathons"],
+  interesses: ["Desenvolvimento Full-Stack", "RPA", "QA"],
   github: "https://github.com/Myoui-sys",
   linkedin: "https://www.linkedin.com/in/dacyrrose-melo/"
 },
-    //Aluno 5
+    //Aluno 6
 { 
+  foto: "img/evellyn.jpg",
   nome: "Evellyn Amelia Rodrigues Gomes",
-  formacoes: ["Análise e Desenvolvimento de Sistemas", "Publicidade e Propaganda"],
-  experiencia: "Designer UX/UI, Marketing Digital",
-  interesses: ["Desenvolvimento Full-Stack, Designer Sênior"],
+  formacoes: ["Análise e Desenvolvimento de Sistemas, Publicidade e Propaganda"],
+  experiencia: ["Designer UX/UI", "Marketing Digital"],
+  interesses: ["Desenvolvimento Full-Stack","Designer Sênior"],
   github: "evellynamelia (Evelyn Amélia)",
   linkedin: "https://www.linkedin.com/in/evellynamélia"
 },
-    //Aluno 6
+    //Aluno 7
 { 
+  foto: "img/lucas.jpg",
   nome: "Lucas Leite de Andrade Porto Americo",
   formacoes: ["Estudante de Programação Front-end (em andamento)"],
-  experiencia: "Design UX e projetos de Front-end",
-  interesses: ["Informática, Design e administração"],
+  experiencia: ["Design UX e projetos de Front-end"],
+  interesses: ["Informática","Design","administração"],
   github: "lucasandrade21porto-ops (lucaaasleite)",
   linkedin: "Lucas Leite | LinkedIn"
 },
-    // Aluno 7 incompleto
+    // Aluno 8 incompleto
 {
+  foto: "img/marcio.jpg",
   nome: "Márcio Eduardo Carmo de Souza",
   formacoes: ["Tec em informática e estudando programação Front End"],
-  experiencia: "Entendimento em hardware e software",
+  experiencia: ["Entendimento em hardware", "software"],
   interesses: ["Artes Marciais, Informatica e UFC."],
   github: "",
   linkedin: ""
 },
-    //aluno  7 incompleto
+    //aluno  9 incompleto
 { 
+  foto: "img/mariaelisa.jpg",
   nome: "Maria Elisa Andrade de Santana",
   formacoes: ["Estudante de programação front end(em andamento)"],
-  experiencia: "Projetos front end",
+  experiencia: ["Projetos front end"],
   interesses: [" Design"],
   github: "",
   linkedin: ""
 },
-    //Aluno 8 
+    //Aluno 10
 {
-
+  foto: "img/matheusmarques.jpg",
   nome: "Matheus Marques dos Santos",
   formacoes: ["Análise e desenvolvimento de sistemas (em andamento)"],
-  experiencia: "Apenas projetos acadêmicos na área de desenvolvimento e engenharia de software",
+  experiencia: ["Apenas projetos acadêmicos na área de desenvolvimento", "Engenharia de software"],
   interesses: [" backend"],
   github: " Matheus-marques2",
   linkedin: " Matheus Marques dos Santos | LinkedIn"
 },
-     //Aluno 9
-{
-    
+     //Aluno 11
+{ 
+  foto: "img/sabrina.jpg",
   nome: "Sabrina Vitoria de Barros Nascimento",
   formacoes: ["Análise e Desenvolvimento de Sistemas (ADS)"],
-  experiencia: " Apenas projetos",
+  experiencia: [" Apenas projetos"],
   interesses: ["Backend"],
   github: " https://github.com/Sabrinabrr",
   linkedin: "https://www.linkedin.com/in/sabrina-nascimento-4840a8234/"
 },
-    //Aluno 10
+    //Aluno 12
 {
-    
+  foto: "img/vitorgabriel.jpg",
   nome: "Vitor Gabriel Dantas Cruz de Oliveira",
   formacoes: [" Ensino Medio Em Andamento"],
-  experiencia: "Desenvolvimento em Front End",
+  experiencia: ["Desenvolvimento em Front End"],
   interesses: ["Informática"],
   github: "https://github.com/02Vitinho",
   linkedin: "https://www.linkedin.com/in/gabriel-dantas-93b3b5412/"
 },
-
-
+    //Aluno 13 incompleto
 {
-    
-  nome: "",
-  formacoes: [""],
-  experiencia: "",
-  interesses: [""],
+  foto: "img/mariahelena.jpg",
+  nome: "Maria Helena Chaves Gomes",
+  formacoes: ["Tecnólogo em Análise de desenvolvimento em sistemas", "Pedagogia (em andamento)"],
+  experiencia: ["Projetos acadêmicos na área de desenvolvimento front end","Estágios na área de pedagogia"],
+  interesses: ["desing"],
   github: "",
   linkedin: ""
 },
 
-
+    //Aluno 14
 {
-    
-  nome: "",
-  formacoes: [""],
-  experiencia: "",
-  interesses: [""],
-  github: "",
-  linkedin: ""
+  foto: "img/messias.jpg",
+  nome: "Messias Kaynã Ferreira de Souza",
+  formacoes: ["Ciências da computação "],
+  experiencia: ["Desenvolvedor front-end", "Análise e desenvolvimento de sistemas (em andamento)", "Redes"],
+  interesses: [" Cybersecurity'", "desenvolvedor Full-Stack"],
+  github: "https://github.com/messiaskayna7",
+  linkedin: "https://www.linkedin.com/in/messias-kayna-7741a4352/"
 },
 
 
