7664f26046ff40ba91f14532b99efd82c0e823fd
Carlos Henrique <carlhenriquedev@gmail.com>
2026-08-16 18:52:40 -0300

    correcao do path de fotos nos dados dos alunos

---
+++ src/data/dados_alunos.js
@@ -0,0 +1,243 @@
+// Lista de alunos
+const alunos = [
+
+  {
+    foto: "../img/alunos/adriele.jpg",
+    nome: "Adrielly Alexandra Cordeiro Velozo",
+    formacoes: ["Análise e Desenvolvimento de Sistemas"],
+    experiencia: ["Hackathon", "Auditoria de Qualidade"],
+    interesses: ["Back-End Developer", "Cybersegurança", "Design"],
+    github: "https://github.com/adriellyvelozotech",
+    linkedin: "https://www.linkedin.com/in/adriellyvelozotech/"
+  },
+  
+  {
+    foto:"../img/alunos/andrea.jpg",
+    nome: "Andréa Verônica Silva Menezes",
+    formacoes: ["Informática Básica","Desenvolvimento de Sistemas"],
+    experiencia: ["Marketing Digital"],
+    interesses: ["Desenvolvimento de Sistemas","Design"],
+    github: "Andreaveronica20003-fonte (Andréasilvaa)",
+    linkedin: "https://www.linkedin.com/in/adriellyvelozotech/"
+  },
+
+  {
+    foto: "../img/alunos/caio.jpg",
+    nome: "Caio Cesar Costa",
+    formacoes: ["Ciência da Computação"],
+    experiencia: ["Hackathons", "Ideathons","Projetos Frontend"],
+    interesses: ["Fullstack development "],
+    github: "https://www.linkedin.com/in/caioccesar/",
+    linkedin: "https://github.com/caiooozs"
+  },
+
+  {   
+    foto: "../img/alunos/carlos.jpg",
+    nome: "Carlos Henrique Correia dos Santos",
+    formacoes: ["Técnico em Desenvolvimento de Sistemas", "Design Gráfico", "Graduando Ciência da Computação"],
+    experiencia: ["Professor de DevOps"],
+    interesses: ["Back-end developer", "DevOps engineer"],
+    github: "https://github.com/carlhenriquex",
+    linkedin: "https://www.linkedin.com/in/carloshenriquex"
+  },
+
+  {
+    foto: "../img/alunos/dacyrrose.jpg",
+    nome: "Dacyrrôse Pessoa Salvador de Melo",
+    formacoes: ["Análise e Desenvolvimento de Sistemas"],
+    experiencia: ["Hackathons", "Ideathons"],
+    interesses: ["Desenvolvimento Full-Stack", "RPA", "QA"],
+    github: "https://github.com/Myoui-sys",
+    linkedin: "https://www.linkedin.com/in/dacyrrose-melo/"
+  },
+
+  { 
+    foto: "../img/alunos/evellyn.jpg",
+    nome: "Evellyn Amelia Rodrigues Gomes",
+    formacoes: ["Análise e Desenvolvimento de Sistemas, Publicidade e Propaganda"],
+    experiencia: ["Designer UX/UI", "Marketing Digital"],
+    interesses: ["Desenvolvimento Full-Stack","Designer Sênior"],
+    github: "evellynamelia (Evelyn Amélia)",
+    linkedin: "https://www.linkedin.com/in/evellynamélia"
+  },
+
+  { 
+    foto: "../img/alunos/lucas.jpg",
+    nome: "Lucas Leite de Andrade Porto Americo",
+    formacoes: ["Estudante de Programação Front-end (em andamento)"],
+    experiencia: ["Design UX e projetos de Front-end"],
+    interesses: ["Informática","Design","administração"],
+    github: "lucasandrade21porto-ops (lucaaasleite)",
+    linkedin: "Lucas Leite | LinkedIn"
+  },
+
+  {
+    foto: "../img/alunos/marcio.jpg",
+    nome: "Márcio Eduardo Carmo de Souza",
+    formacoes: ["Tec em informática e estudando programação Front End"],
+    experiencia: ["Entendimento em hardware", "software"],
+    interesses: ["Artes Marciais, Informatica e UFC."],
+    github: "https://github.com/marcioart878-stack",
+    linkedin: "https://www.linkedin.com/in/márcio-eduardo-862440302"
+  },
+
+  { 
+    foto: "../img/alunos/elisa.jpg",
+    nome: "Maria Elisa Andrade de Santana",
+    formacoes: ["Estudante de programação front end(em andamento)"],
+    experiencia: ["Projetos front end"],
+    interesses: [" Design"],
+    github: "https://github.com/lisalizz2110-svg",
+    linkedin: "https://www.linkedin.com/in/maria-elisa-andrade-7b36b83a1/"
+  },
+
+  {
+    foto: "../img/alunos/matheus.jpg",
+    nome: "Matheus Marques dos Santos",
+    formacoes: ["Análise e desenvolvimento de sistemas (em andamento)"],
+    experiencia: ["Apenas projetos acadêmicos na área de desenvolvimento", "Engenharia de software"],
+    interesses: [" backend"],
+    github: " Matheus-marques2",
+    linkedin: " Matheus Marques dos Santos | LinkedIn"
+  },
+
+  { 
+    foto: "../img/alunos/sabrina.jpg",
+    nome: "Sabrina Vitoria de Barros Nascimento",
+    formacoes: ["Análise e Desenvolvimento de Sistemas (ADS)"],
+    experiencia: [" Apenas projetos"],
+    interesses: ["Backend"],
+    github: " https://github.com/Sabrinabrr",
+    linkedin: "https://www.linkedin.com/in/sabrina-nascimento-4840a8234/"
+  },
+
+  {
+    foto: "../img/alunos/vitor.jpg",
+    nome: "Vitor Gabriel Dantas Cruz de Oliveira",
+    formacoes: [" Ensino Medio em andamento"],
+    experiencia: ["Desenvolvimento em Front End"],
+    interesses: ["Informática"],
+    github: "https://github.com/02Vitinho",
+    linkedin: "https://www.linkedin.com/in/gabriel-dantas-93b3b5412/"
+  },
+
+  {
+    foto: "../img/alunos/helena.jpg",
+    nome: "Maria Helena Chaves Gomes",
+    formacoes: ["Tecnólogo em Análise de desenvolvimento em sistemas", "Pedagogia (em andamento)"],
+    experiencia: ["Projetos acadêmicos na área de desenvolvimento front end","Estágios na área de pedagogia"],
+    interesses: ["desing"],
+    github: "https://github.com/lisalizz2110-svg",
+    linkedin: "https://www.linkedin.com/in/maria-helena-chaves-33296b372"
+  },
+
+  {
+    foto: "../img/alunos/messias.jpg",
+    nome: "Messias Kaynã Ferreira de Souza",
+    formacoes: ["Ciências da computação "],
+    experiencia: ["Desenvolvedor front-end", "Análise e desenvolvimento de sistemas (em andamento)", "Redes"],
+    interesses: [" Cybersecurity'", "desenvolvedor Full-Stack"],
+    github: "https://github.com/messiaskayna7",
+    linkedin: "https://www.linkedin.com/in/messias-kayna-7741a4352/"
+  },
+
+  {
+    foto: "../img/alunos/danilo.jpg",
+    nome: "Danilo de Freitas da Silva",
+    formacoes: ["Auxiliar administrativo", "desenvolvedor Front-End", "técnico administrativo (em andamento)"],
+    experiencia: ["Atendimento ao cliente", "recepção", "vendedor"],
+    interesses: [" Engenharia mecânica", "engenharia automotiva", "marketing"],
+    github: "https://github.com/danilodf254280-ctrl",
+    linkedin: "https://www.linkedin.com/in/danilofreitas08/"
+  },
+
+  {
+    foto: "../img/alunos/edinaldo.jpg",
+    nome: "Edinaldo da Silva Ramos",
+    formacoes: ["Estudante de Programação Front-end (em andamento)"],
+    experiencia: ["Designer UI"],
+    interesses: ["Desenvolvimento front-end"," Design UI"],
+    github: "https://github.com/edinaldodasilvaramoss-collab",
+    linkedin: "https://www.linkedin.com/in/edinaldo-ramos-a05785412"
+  },
+
+  {
+    foto: "../img/alunos/ewerton.jpg",
+    nome: "Ewerton Henrique Lima da Silva",
+    formacoes: ["Montagem e Manutenção de Computadores", "Notebooks", "Gestão Empresarial com Software ERP", "Programação Front-end (em andamento)"],
+    experiencia: ["Suporte de TI", "Auxiliar administrativo"],
+    interesses: ["Desenvolvedor Fullstack"],
+    github: "EwertonHenriquex (Ewerton Henrique) ",
+    linkedin: "Ewerton Silva | LinkedIn"
+  },
+
+  {   
+    foto: "../img/alunos/flavio.jpg",
+    nome: "Flávio Aureliano Cordeiro Coutinho Filho",
+    formacoes: ["Análise de dados", "Desenvolvimento de sistemas", "Suporte técnico", "Help desk"],
+    experiencia: ["Jovem aprendiz TI", "Estágio suporte interno TI"],
+    interesses: ["Cibersegurança"],
+    github: "https://github.com/flavioaccf ",
+    linkedin: "https://www.linkedin.com/in/fl%C3%A1vio-aureliano-dev/"
+  },
+
+  {
+    foto: "../img/alunos/isabely.jpg",
+    nome: "Isabely Cabral Sales",
+    formacoes: ["Tecnica de logística, Tecnica de administração", "Cursando tecnólogo de Gestão financeira"," Cursando programação Front-end. (em andamento) "],
+    experiencia: ["projetos de Front-end.", "administração", "área financeira"],
+    interesses: ["Desenvolvimento front-end"],
+    github: "https://github.com/ISABELY430",
+    linkedin: "https://www.linkedin.com/in/isabely-cabral-034915267"
+  },
+
+  {
+    foto: "../img/alunos/eduarda.jpg",    
+    nome: "Maria Eduarda de Sousa Santana",
+    formacoes: ["Ciências Contábeis"],
+    experiencia: ["Analista Fiscal "],
+    interesses: ["  Análise de Dados e Desing Web"],
+    github: "",
+    linkedin: ""
+  },
+
+  {
+    foto: "../img/alunos/thamires.jpg",
+    nome: "Thamires Vitória Muniz da Silva",
+    formacoes: [" Tecnólogo em Análise de desenvolvimento em sistemas (em andamento)"],
+    experiencia: ["Experiência em projetos acadêmicos pelo Porto Digital", "DIO"," curso de Análise e Desenvolvimento de Sistemas pela ETE"],
+    interesses: ["FullStack"],
+    github: "https://github.com/Thamiresvitoria ",
+    linkedin: "https://www.linkedin.com/in/thamires-vitoria-muniz-da-silva-7b3878352/"
+  },
+
+  {
+    foto:"../img/alunos/kawanne.jpg",
+    nome: "Kawanne Ketllyn da Silva de Santana",
+    formacoes: ["Ensino médio completo"],
+    experiencia: [" Desenvolvimento front-end. (em andamento)"],
+    interesses: ["Fullstack"],
+    github: "",
+    linkedin: "https://www.linkedin.com/in/kawanne-ketllyn-92b596172/"
+  },
+
+  {
+    foto: "../img/alunos/joao.jpg",
+    nome:"Joao Vitor Atila Gomes dos Santos",
+    formacoes: ["Análise e desenvolvimento (em andamento)", "Suporte técnico", "Eletrônica básica"],
+    experiencia: [ "Consertos de eletrodomésticos", "Computadores", "Controles e Componentes", "Desenvolvimento básico na área de Java script"], 
+    interesses: ["Desenvolver mais na área de sistemas e fazer Network"],
+    github: "",
+    linkedin: "https://www.linkedin.com/in/joão-vitor-157b5b361/"
+  },
+
+  {
+    foto:"../img/alunos/mariaeduardafe.jpg",
+    nome: "Maria Eduarda Ferreira da Silva",
+    formacoes: ["Ensino médio completo"],
+    experiencia: "",
+    interesses: [""],
+    github: "",
+    linkedin: ""
+  }
+];
